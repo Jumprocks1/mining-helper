3509d22a5f55f65df34f60d13021cf072c2129f3
Jumprocks <jumprocks1@gmail.com>
2025-09-28 10:07:43 -0700

    Remove csv/zip saver

--- src/jpdb-content.ts
+++ src/jpdb-content.ts
@@ -41,106 +41,14 @@ async function requestAudio(audios) {
     })
 }
 
-
-/**
- * @param {Blob} blob
- * @param {string} name 
- */
-const downloadBlob = async (blob, name) => {
-    const url = URL.createObjectURL(blob)
-    const a = document.createElement('a')
-    a.href = url
-    a.download = name
-    a.click()
-    setTimeout(() => URL.revokeObjectURL(url), 1000)
-}
-
-/** @param {string} s */
-const sound = s => `[sound:${s}]`
-
-/** @param {CardData[]} cards */
-function csv(cards) {
-    /** @type {{name:string, get: (e: CardData) => string}[]} */
-    const fields = [
-        { name: "Word", get: e => e.kanji },
-        { name: "Word Reading", get: e => furiToReading(e.furigana) },
-        { name: "Word Meaning", get: e => e.meaning },
-        { name: "Word Furigana", get: e => e.furigana },
-        { name: "Word Audio", get: e => sound(e.audioLocalFile) },
-        { name: "Sentence", get: e => e.jpSentenceKanji },
-        { name: "Sentence Meaning", get: e => e.enSentence },
-        { name: "Sentence Furigana", get: e => e.jpSentenceFuri },
-        { name: "Sentence Audio", get: e => sound(e.sentenceAudioLocalFile) },
-        // TODO add sentence hash as ID or something
-    ]
-    let o = ""
-    o += "#separator:Pipe\n"
-    o += "#html:true\n"
-    o += "#columns:"
-    for (let i = 0; i < fields.length; i++) {
-        o += fields[i].name
-        if (i < fields.length - 1) o += "|"
-    }
-    o += "\n"
-    for (const card of cards) {
-        for (let i = 0; i < fields.length; i++) {
-            o += fields[i].get(card)
-            if (i < fields.length - 1) o += "|"
-        }
-        o += "\n"
-    }
-    return o
-}
-
-// TODO should move this to service worker?
-async function downloadPendingCards() {
-    const keys = await chrome.storage.session.getKeys()
-    const cardsObject = await chrome.storage.session.get(keys)
-    chrome.storage.session.clear()
-    /** @type {CardData[]} */
-    const cards = []
-    for (const key in cardsObject)
-        cards.push(cardsObject[key])
-
-
-    const csvString = csv(cards)
-    const zipFileWriter = new zip.BlobWriter();
-    const zipWriter = new zip.ZipWriter(zipFileWriter);
-
-    await zipWriter.add("cards.csv", new zip.TextReader(csvString))
-
-    for (const card of cards) {
-        if (card.audioBytes)
-            await zipWriter.add(card.audioLocalFile, new zip.BlobReader(new Blob([card.audioBytes])))
-        if (card.sentenceAudioBytes)
-            await zipWriter.add(card.sentenceAudioLocalFile, new zip.BlobReader(new Blob([card.sentenceAudioBytes])))
-    }
-
-    await zipWriter.close();
-    await downloadBlob(await zipFileWriter.getData(), "jpdb.zip")
-}
-
-document.addEventListener("keypress", e => {
-    if (e.ctrlKey && e.shiftKey && e.code === "KeyS") {
-        downloadPendingCards()
-    }
-})
-
-
 const css = document.createElement("style")
 css.innerHTML = style
 target.prepend(css)
 
-zip.configure({ useWebWorkers: false }) // these break everything, not sure why
-
-/**
- * @param {string} css
- * @param {HTMLElement} node
- */
-function select(css, node) {
+function select(css: string, node: HTMLElement | undefined) {
     const all = document.querySelectorAll(css)
     for (const e of all) e.classList.remove("selected")
-    node.classList.add("selected")
+    if (node) node.classList.add("selected")
 }
 
 async function afterLoad() {
--- src/ss-content.ts
+++ src/ss-content.ts
@@ -1,3 +1,5 @@
+import { CardData, lookupFuri, urlToArrayBuffer } from "./util";
+
 export { }
 
 const sentenceCss = "#search-results-list>.search-result"
@@ -16,78 +18,22 @@ const css = document.createElement("style")
 css.innerHTML = style;
 (document.head || document.documentElement).prepend(css)
 
-/** @param {CardData} card */
-async function lookupFuri(card) {
-    const s = card.jpSentenceKanji
-    const res = await fetch("https://jpdb.io/api/v1/parse", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            text: s,
-            token_fields: [
-                "position",
-                "length",
-                "furigana"
-            ],
-            position_length_encoding: "utf16"
-        })
-    })
-    const json = await res.json()
-    // TODO we could get the furi for the kanji, but for now that will come from jpdb mining
-    // const targetLeft = card.jpSentenceKanji.indexOf(card.kanji)
-    // const targetRight = targetLeft + card.kanji.length
-    let wordFuri = ""
-    let o = ""
-    for (const token of json.tokens) {
-        const [position, length, furi] = token
-        const left = position
-        const right = position + length
-        if (furi === null) {
-            o += s.substring(position, position + length)
-        }
-        else {
-            for (const part of furi) {
-                if (!Array.isArray(part)) {
-                    o += part
-                } else {
-                    if (o.length > 0) {
-                        const prev = o[o.length - 1]
-                        if (prev !== "]" && prev !== ">")
-                            o += " "
-                    }
-                    o += `${part[0]}[${part[1]}]`
-                }
-            }
-        }
-    }
-    card.jpSentenceFuri = o
-    if (!card.furigana) card.furigana = wordFuri
-}
-
-async function urlToArrayBuffer(url) {
-    // TODO skip CORS with header mod
-    const res = await fetch(url)
-    return await (await res.blob()).arrayBuffer()
-}
-
 async function updateCard() {
     const sentenceElement = document.querySelector(sentenceCss + ".selected") ?? document.querySelector(sentenceCss)
-    const searchInput = document.getElementById("searchInput")
+    const searchInput = document.getElementById("searchInput") as HTMLInputElement
+    if (!sentenceElement || !searchInput) return
+
     const kanji = searchInput.value
-    /** @type {CardData} */
-    const card = (await chrome.storage.session.get({ [kanji]: {} }))[kanji]
+    const card: CardData = (await chrome.storage.session.get({ [kanji]: {} }))[kanji]
     card.kanji = kanji
-    card.jpSentenceKanji = sentenceElement.querySelector(".jap").textContent
-    card.enSentence = sentenceElement.querySelector(".eng").textContent
-    const audioUrl = sentenceElement.querySelector("a.audioDownload").href
+    card.jpSentenceKanji = sentenceElement.querySelector(".jap")?.textContent
+    card.enSentence = sentenceElement.querySelector(".eng")?.textContent
+    const audioUrl = sentenceElement.querySelector<HTMLAnchorElement>("a.audioDownload")?.href
     card.sentenceAudioBytes = await urlToArrayBuffer(audioUrl)
     card.sentenceAudioLocalFile = `${card.kanji}_ex_sentencesearch.ogg`
     card.sentenceIndex = "ss_" + [...document.querySelectorAll(sentenceCss)].indexOf(sentenceElement)
 
-    await lookupFuri(card)
+    card.jpSentenceFuri = await lookupFuri(card.jpSentenceKanji)
 
     console.log(card)
     chrome.storage.session.set({ [kanji]: card })
