704004eee205666e6eae5e7836fff1384e85acfb
Jumprocks <jumprocks1@gmail.com>
2025-10-05 16:26:34 -0700

    Better highlighting

--- src/subtitles/SubtitleViewer.tsx
+++ src/subtitles/SubtitleViewer.tsx
@@ -1,42 +1,100 @@
 import { formatTimestamp, Subtitles } from "../utils/srt"
 import { oldCreateElement } from "../utils/util"
 
-function updateBlock(block: HTMLElement, subtitles: Subtitles) {
-    const inner = block.querySelector(":scope > .inner")
-    if (!inner) return
-    const newChildren: Node[] = []
-    for (const entry of subtitles.entries) {
-        newChildren.push(entry.node = oldCreateElement("div", {
-            className: "subtitle-entry",
-            children: [
-                oldCreateElement("span", {
-                    className: "timestamp",
-                    textContent: formatTimestamp(entry.startTime),
-                    mutate: (e: any) => e.entry = entry
-                }),
-                oldCreateElement("div", {
-                    className: "subtitles",
-                    children: [
-                        oldCreateElement("span", {
-                            className: "main-subtitle",
-                            textContent: entry.text,
-                            data: {
-                                tooltip: entry.translation
-                            }
-                        }),
-                    ]
-                })
-            ]
-        }))
-    }
-    inner.replaceChildren(...newChildren)
-}
+export default class SubtitleViewer {
+    Node: HTMLElement
+    subtitles: Subtitles
+    main: boolean
+    pointer: HTMLElement = <div className="pointer">-&gt;</div>
+    static readonly MAIN_ID = "main-subtitles";
 
-export default (subtitles: Subtitles) => {
-    const res = <div className="subtitle-viewer">
-        <div className="pointer">-&gt;</div>
-        <div className="inner"></div>
-    </div>
-    updateBlock(res, subtitles);
-    return res
+
+
+    constructor(subtitles: Subtitles, main: boolean) {
+        this.main = main
+        this.Node = <div className="subtitle-viewer">
+            {this.pointer}
+            <div className="inner"></div>
+        </div>
+        if (main) this.Node.id = SubtitleViewer.MAIN_ID
+        this.subtitles = subtitles
+        this.updateBlock()
+    }
+
+    updateBlock() {
+        const inner = this.Node.querySelector(":scope > .inner")
+        if (!inner) return
+        const newChildren: Node[] = []
+        for (const entry of this.subtitles.entries) {
+            newChildren.push(entry.node = oldCreateElement("div", {
+                className: "subtitle-entry",
+                children: [
+                    oldCreateElement("span", {
+                        className: "timestamp",
+                        textContent: formatTimestamp(entry.startTime),
+                        mutate: (e: any) => e.entry = entry
+                    }),
+                    oldCreateElement("div", {
+                        className: "subtitles",
+                        children: [
+                            oldCreateElement("span", {
+                                className: "main-subtitle",
+                                textContent: entry.text,
+                                data: {
+                                    tooltip: entry.translation
+                                }
+                            }),
+                        ]
+                    })
+                ]
+            }))
+        }
+        inner.replaceChildren(...newChildren)
+    }
+
+
+    UpdateHighlighting(currentTime: number) {
+        const scroll = document.getElementById("body-container")
+        if (!scroll) return
+        for (let i = 0; i < this.subtitles.entries.length; i++) {
+            const entry = this.subtitles.entries[i]
+            const node = entry.node
+            if (node) {
+                if (entry.startTime <= currentTime && entry.endTime > currentTime) {
+                    this.pointer.style.top = node.offsetTop + node.offsetHeight / 2 + "px"
+                    break
+                }
+                if (entry.startTime > currentTime) {
+                    const prevEntry = i > 0 ? this.subtitles.entries[i - 1] : undefined
+                    const prevTime = prevEntry?.endTime ?? 0
+                    const prevPos = (prevEntry && prevEntry.node) ?
+                        prevEntry.node.offsetTop + prevEntry.node.clientHeight / 2 : 0
+                    const a = (currentTime - prevTime) / (entry.startTime - prevTime)
+                    this.pointer.style.top = prevPos + (node.offsetTop + node.offsetHeight / 2 - prevPos) * a + "px"
+                    break
+                }
+            }
+        }
+        for (const entry of this.subtitles.entries) {
+            const node = entry.node
+            if (node) {
+                if (entry.startTime <= currentTime && entry.endTime > currentTime) {
+                    // only scroll if we add a new highlight
+                    if (!node.classList.contains("highlight")) {
+                        node.classList.add("highlight")
+                        if (this.main) {
+                            const center = node.offsetTop + node.offsetHeight / 2;
+                            const scrollHeight = scroll.clientHeight
+                            if (center < scroll.scrollTop + scroll.clientHeight / 6
+                                || center > scroll.scrollTop + scroll.clientHeight * (1 - 1 / 6)) {
+                                scroll.scrollTo({ top: center - scrollHeight / 6 });
+                            }
+                        }
+                    }
+                } else {
+                    node.classList.remove("highlight")
+                }
+            }
+        }
+    }
 }
--- src/subtitles/mpv.tsx
+++ src/subtitles/mpv.tsx
@@ -13,79 +13,30 @@ function updateTime(timestamp: number) {
     if (timeElement)
         timeElement.textContent = formatTimestamp(timestamp)
 
-    updateHighlighting(loadedSubtitles.main)
-    loadedSubtitles.secondary.forEach(updateHighlighting)
+    loadedSubtitles.main?.UpdateHighlighting(currentTime)
+    loadedSubtitles.secondary.forEach(e => e.UpdateHighlighting(currentTime))
 }
-
-function updateHighlighting(subtitles?: Subtitles) {
-    const scroll = document.getElementById("body-container")
-    if (!subtitles || !scroll) return
-    const main = loadedSubtitles.main === subtitles
-    const pointer = scroll.querySelector<HTMLElement>(main ? "#main-subtitles .pointer" : "#secondary-subtitles .pointer")!
-    for (let i = 0; i < subtitles.entries.length; i++) {
-        const entry = subtitles.entries[i]
-        const node = entry.node
-        if (node) {
-            if (entry.startTime <= currentTime && entry.endTime >= currentTime) {
-                pointer.style.top = node.offsetTop + node.offsetHeight / 2 + "px"
-                break
-            }
-            if (entry.startTime > currentTime) {
-                const prevEntry = i > 0 ? subtitles.entries[i - 1] : undefined
-                const prevTime = prevEntry?.endTime ?? 0
-                const prevPos = (prevEntry && prevEntry.node) ?
-                    prevEntry.node.offsetTop + prevEntry.node.clientHeight / 2 : 0
-                const a = (currentTime - prevTime) / (entry.startTime - prevTime)
-                pointer.style.top = prevPos + (node.offsetTop + node.offsetHeight / 2 - prevPos) * a + "px"
-                break
-            }
-        }
-    }
-    for (const entry of subtitles.entries) {
-        const node = entry.node
-        if (node) {
-            if (entry.startTime <= currentTime && entry.endTime >= currentTime) {
-                // only scroll if we add a new highlight
-                if (!node.classList.contains("highlight")) {
-                    node.classList.add("highlight")
-                    if (main) {
-                        const center = node.offsetTop + node.offsetHeight / 2;
-                        const scrollHeight = scroll.clientHeight
-                        if (center < scroll.scrollTop + scroll.clientHeight / 6
-                            || center > scroll.scrollTop + scroll.clientHeight * (1 - 1 / 6)) {
-                            scroll.scrollTo({ top: center - scrollHeight / 6 });
-                        }
-                    }
-                }
-            } else {
-                node.classList.remove("highlight")
-            }
-        }
-    }
-}
+// @ts-expect-error
+window.updateTime = updateTime
 
 const loadedSubtitles: {
-    main?: Subtitles
-    secondary: Subtitles[]
+    main?: SubtitleViewer
+    secondary: SubtitleViewer[]
 } = { secondary: [] }
 
 function loadSubtitles(subtitles: Subtitles, main: boolean) {
     const container = document.getElementById("subtitle-container")
     if (!container) return
 
-    const mainId = "main-subtitles"
 
-    if (main) {
-        const existing = document.getElementById(mainId)
-        if (existing) existing.remove()
-    }
+    if (main) loadedSubtitles.main?.Node.remove()
 
-    const block = SubtitleViewer(subtitles)
-    if (main) block.id = mainId
-    container.append(block)
+    const viewer = new SubtitleViewer(subtitles, main)
+    container.append(viewer.Node)
+    viewer.UpdateHighlighting(currentTime)
 
-    if (main) loadedSubtitles.main = subtitles
-    else loadedSubtitles.secondary.push(subtitles)
+    if (main) loadedSubtitles.main = viewer
+    else loadedSubtitles.secondary.push(viewer)
 }
 async function handleWebSocketData(command: string | Blob) {
     if (typeof command === "string") {
@@ -167,7 +118,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 const entry = (clicked as any).entry
                 if (!entry) return
                 const time = entry.startTime
-                webSocket.OpenAndSend(`ipc:seek ${time / 1000} absolute`)
+                if (webSocket.Open) webSocket.SendIfOpen(`ipc:seek ${time / 1000} absolute`)
+                else updateTime(time)
                 ev.preventDefault();
             } else if (clicked.matches("#connection-status-dot.disconnected")) {
                 webSocket.Connect();
