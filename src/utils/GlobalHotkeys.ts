994c883dfcc18dd359b5594b6b97ab7bf070a278
Jumprocks <jumprocks1@gmail.com>
2025-10-10 20:38:55 -0700

    openTab helper

--- src/utils/GlobalHotkeys.ts
+++ src/utils/GlobalHotkeys.ts
@@ -1,22 +1,30 @@
 import { UnicodeCharacterType, unicodeType } from "../anki/CardList";
 
+export function openTab(url: string) {
+    if (chrome.tabs) {
+        chrome.tabs.create({ url })
+    } else {
+        window.open(url, "_blank", "noopener,noreferrer");
+    }
+}
+
 export function keyPressedWithText(ev: KeyboardEvent, text: string) {
     if (!text) return
     if (ev.key === "j") {
-        chrome.tabs.create({ url: `https://jisho.org/search/${encodeURIComponent(text)}` });
+        openTab(`https://jisho.org/search/${encodeURIComponent(text)}`)
         return true
     } else if (ev.key == "d") {
         const isSingleKanji = text.length === 1 && unicodeType(text) === UnicodeCharacterType.Kanji
         if (isSingleKanji) {
-            chrome.tabs.create({ url: `https://jpdb.io/kanji/${encodeURIComponent(text)}` });
+            openTab(`https://jpdb.io/kanji/${encodeURIComponent(text)}`)
             return true
         }
         else {
-            chrome.tabs.create({ url: `https://jpdb.io/search?q=${encodeURIComponent(text)}&lang=english` });
+            openTab(`https://jpdb.io/search?q=${encodeURIComponent(text)}&lang=english`)
             return true
         }
     } else if (ev.key === "s") {
-        chrome.tabs.create({ url: `https://sentencesearch.neocities.org/#${encodeURIComponent(text)}` });
+        openTab(`https://sentencesearch.neocities.org/#${encodeURIComponent(text)}`)
         return true
     }
 }
