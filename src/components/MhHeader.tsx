0ec9d857a533264a5f2c16ffef767d07b62ea841
Jumprocks <jumprocks1@gmail.com>
2025-10-12 16:27:19 -0700

    Finish SS page for now

--- src/components/MhHeader.tsx
+++ src/components/MhHeader.tsx
@@ -1,11 +1,8 @@
-import { seed } from "./util"
-
 export default () => {
-    const header = <div id="mh-header">
+    return <div id="mh-header">
         <a className="button home" id="title" href="/popup.html">Mining Helper</a>
         <a className="button subs" href="/subs.html">Subs</a>
         <a className="button anki" href="/anki.html">Anki</a>
         <a className="button ss" href="/ss.html">SS</a>
     </div>
-    return header
 }
--- src/pages/ss/ss.tsx
+++ src/pages/ss/ss.tsx
@@ -1,5 +1,6 @@
 import MhHeader from "../../components/MhHeader"
 import { seedPage } from "../../components/util"
+import { CardData, lookupFuri, urlToArrayBuffer } from "../../utils/util"
 
 
 const dataUrl = chrome.runtime.getURL("all_v11.json")
@@ -12,6 +13,8 @@ interface SsEntry {
     eng: string
 
     search: string
+
+    audioBytes?: Promise<ArrayBuffer | undefined>
 }
 
 const searchInput = <input autocomplete="off" id="search" /> as HTMLInputElement
@@ -29,6 +32,30 @@ const body = <div id="body-container">
     {results}
 </div>
 
+let audioContext: AudioContext | undefined = undefined
+
+let playing: AudioBufferSourceNode[] = []
+
+function stopAll() {
+    playing.forEach(e => e.stop())
+    playing = []
+}
+
+async function play(bytes: ArrayBuffer) {
+    audioContext ??= new AudioContext()
+    const source = audioContext.createBufferSource();
+    // audio thread needs a copy of the buffer, so we have to slice
+    source.buffer = await audioContext.decodeAudioData(bytes.slice(0));
+    source.connect(audioContext.destination);
+
+    stopAll()
+
+    let i = playing.length
+    playing.push(source)
+    source.onended = () => playing = playing.splice(i, 1)
+    source.start()
+}
+
 
 document.addEventListener("DOMContentLoaded", async () => {
     seedPage("ss-page", [
@@ -41,6 +68,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     data.forEach(e => {
         e.search = (e.eng + " " + e.jap).toLowerCase()
     })
+
+
+    async function updateCard(entry: SsEntry) {
+        const keys = await chrome.storage.session.getKeys()
+        const query = searchInput.value.trim()
+        let kanji = query
+        const foundKey = keys.find(e => e.includes(kanji))
+        if (foundKey) kanji = foundKey
+
+        const card: CardData = (await chrome.storage.session.get({ [kanji]: {} }))[kanji]
+        card.kanji = kanji
+        card.jpSentenceKanji = entry.jap
+        card.enSentence = entry.eng
+
+        card.sentenceAudioBytes = await entry.audioBytes
+        card.sentenceAudioLocalFile = `${card.kanji}_ex_sentencesearch.ogg`
+        card.sentenceIndex = "ss_" + data.indexOf(entry)
+
+        card.jpSentenceFuri = await lookupFuri(card.jpSentenceKanji, query)
+        card.jpSentenceKanji = card.jpSentenceKanji.replace(query, "<b>" + query + "</b>")
+
+        console.log(card)
+        chrome.storage.session.set({ [kanji]: card })
+    }
 
     function update() {
         const query = searchInput.value.trim()
@@ -59,11 +110,25 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         let i = 0;
         for (const res of matching) {
-            // TODO add audio + keyword highlighting
-            results.appendChild(<div className="match">
-                <span className="jap">{res.jap}</span>
+            const highlighted = res.jap.replace(query, "<b>" + query + "</b>")
+            const jap = <span className="jap"></span>
+            jap.innerHTML = highlighted
+            const row = <div className="match">
+                {jap}
                 <span className="eng">{res.eng}</span>
-            </div>)
+            </div>
+            results.appendChild(row)
+            row.addEventListener("click", async () => {
+                results.querySelectorAll(".selected").forEach(e => e.classList.remove("selected"))
+                const audioBaseUrl = "https://receptomanijalogi.web.app/audio/"
+                row.classList.add("selected")
+
+                res.audioBytes ??= urlToArrayBuffer(audioBaseUrl + res.audio_jap)
+                const bytes = await res.audioBytes
+                if (bytes) play(bytes)
+
+                updateCard(res)
+            })
             i += 1
             if (i > 100) break
         }
