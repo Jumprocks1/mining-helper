27f0a7c5213d1c86ed61df8f2a7c2d14af0838df
Jumprocks <jumprocks1@gmail.com>
2025-10-24 22:15:44 -0700

    Check for partial card matches when editing/getting

--- src/utils/MiningUtil.tsx
+++ src/utils/MiningUtil.tsx
@@ -1,6 +1,14 @@
 import { CardData } from "./util"
 
-export async function getOrCreatePendingCard(word: string) {
+export async function getOrCreatePendingCard(word: string, checkPartial: boolean) {
+    word = word.trim()
+
+    if (checkPartial) {
+        const keys = await chrome.storage.session.getKeys()
+        const foundKey = keys.find(e => e.includes(word))
+        if (foundKey) word = foundKey
+    }
+
     const res = await chrome.storage.session.get({ [word]: {} })
     const card: CardData = res[word]
     card.kanji = word
@@ -8,8 +16,8 @@ export async function getOrCreatePendingCard(word: string) {
     return card
 }
 
-export async function mutatePendingCard(word: string, mutate: (card: CardData) => Promise<void>) {
-    const card = await getOrCreatePendingCard(word)
+export async function mutatePendingCard(word: string, checkPartial: boolean, mutate: (card: CardData) => Promise<void>) {
+    const card = await getOrCreatePendingCard(word, checkPartial)
     card.modified = Date.now()
     await mutate(card)
     await saveCard(card)
