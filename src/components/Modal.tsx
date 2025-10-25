f50a3da20484484c1e08b54a50309b00138b62f8
Jumprocks <jumprocks1@gmail.com>
2025-10-24 22:03:51 -0700

    Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,70 @@
+import AddIcons from "../utils/AddIcons";
+import { Component } from "./Component";
+
+interface Props {
+    header: any
+    footer?: any
+    onClose: () => void
+    body: HTMLElement | ((body: HTMLElement) => Promise<(string | Node)[]>)
+}
+
+export class Modal extends Component {
+
+    Node: HTMLElement
+
+    IsOpen = false;
+
+    OnClose: () => void
+
+    constructor(props: Props) {
+        super()
+        this.OnClose = props.onClose
+
+        AddIcons()
+        const closeButton = <span className="material-symbols-outlined icon-button close-button">close</span>
+
+        const body = <div className="body"></div>
+
+        let innerBody: HTMLElement
+        if (props.body instanceof Element) {
+            innerBody = props.body
+        } else {
+            innerBody = <div className="loader" />
+            props.body(body).then(e => innerBody.replaceWith(...e))
+        }
+        body.append(innerBody)
+
+        const inner = <div className="inner-modal">
+            <div className="header">
+                <div>{props.header}</div>
+                {closeButton}
+            </div>
+            {body}
+            {props.footer && <div className="footer">{props.footer}</div>}
+        </div>
+        const res = <div className="modal">
+            {inner}
+        </div>
+        res.onclick = ev => {
+            if (ev.target === res) {
+                this.Close()
+            }
+        }
+        closeButton.onclick = this.Close
+        this.Node = res
+    }
+
+    Close() {
+        if (!this.IsOpen) return
+        this.IsOpen = false;
+        this.Node.remove()
+        // it's fine if OnClose calls this Close for some reason since the guard above will prevent a loop
+        this.OnClose()
+    }
+
+    Open() {
+        if (this.IsOpen) return
+        this.IsOpen = true
+        document.body.append(this.Node)
+    }
+}
