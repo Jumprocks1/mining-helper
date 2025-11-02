b3a8f07e5dab77fa5c62b7bd676c08305797e518
Jumprocks <jumprocks1@gmail.com>
2025-11-01 20:41:52 -0700

    Add HotReload to watch script

--- Watch.ps1
+++ Watch.ps1
@@ -1,9 +1,19 @@
+param(
+    [switch]$HotReloadCss
+)
+
 $sass = Start-Process cmd "/c sass --embed-source-map --watch src/main.scss:dist/main.css" -NoNewWindow -PassThru
 $esbuild = Start-Process node esbuild.mjs -NoNewWindow -PassThru
 
 try {
-    Write-Host "Jobs started"
-    Wait-Process -Id $sass.Id,$esbuild.Id
+    if ($HotReloadCss) {
+        $css = Start-Process dotnet "run --project .\HotReload\HotReload.csproj" -NoNewWindow -PassThru
+        Write-Host "Jobs started"
+        Wait-Process -Id $sass.Id,$esbuild.Id,$css.Id
+    } else {
+        Write-Host "Jobs started"
+        Wait-Process -Id $sass.Id,$esbuild.Id
+    }
 } finally {
     Write-Host "Exiting"
     Stop-Process $sass
