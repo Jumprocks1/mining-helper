19da6ea8f29be0868463e8d8d7ab38aa063386c5
Jumprocks <jumprocks1@gmail.com>
2025-11-15 15:25:28 -0700

    Basic Checkbox

---
+++ src/components/basic/CheckboxField.tsx
@@ -0,0 +1,18 @@
+
+interface Props {
+    checked?: boolean
+    onChange: (e: boolean) => void
+    label?: string
+    id: string
+}
+
+export default (props: Props) => {
+    const res = <label htmlFor={props.id} className="checkbox-field">
+        <input id={props.id} type="checkbox" checked={props.checked} onchange={ev => {
+            const target = ev.target as HTMLInputElement
+            props.onChange(target.checked)
+        }} />
+        {props.label}
+    </label>
+    return res
+}
