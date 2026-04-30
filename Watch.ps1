763ee4163375158829cedc32d854048126343478
Jumprocks <jumprocks1@gmail.com>
2026-04-30 16:29:02 -0700

    Fix esbuild with CSS

--- Watch.ps1
+++ Watch.ps1
@@ -6,7 +6,7 @@ $sass = Start-Process cmd "/c sass --embed-source-map --source-map-urls=absolute
 
 try {
     if ($HotReloadCss) {
-        $esbuild = Start-Process node esbuild.mjs,--hot-reload-css -NoNewWindow -PassThru
+        $esbuild = Start-Process node esbuild.mjs,--watch,--hot-reload-css -NoNewWindow -PassThru
         $css = Start-Process dotnet "run --project .\HotReload\HotReload.csproj" -NoNewWindow -PassThru
         Write-Host "Jobs started"
         Wait-Process -Id $sass.Id,$esbuild.Id,$css.Id
